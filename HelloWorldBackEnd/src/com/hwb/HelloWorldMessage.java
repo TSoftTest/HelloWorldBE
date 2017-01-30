be6aeb3b15584479c10f251bb9fd73df9d3190cb
Diego <diegobuzzalino@yahoo.com.ar>
2017-01-30 13:38:54 -0300

    v17

--- HelloWorldBackEnd/src/com/hwb/HelloWorldMessage.java
+++ HelloWorldBackEnd/src/com/hwb/HelloWorldMessage.java
@@ -1,7 +1,7 @@
 package com.hwb;
 
 public class HelloWorldMessage {
-	private String message = "Hello World!!!! - v16";
+	private String message = "Hello World!!!! - v17";
 
 	public String getMessage() {
 		return this.message;
