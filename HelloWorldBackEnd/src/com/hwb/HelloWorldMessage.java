11287e931dfd2282349b202edccacb268d0068dc
Diego <diegobuzzalino@yahoo.com.ar>
2017-02-03 15:50:41 -0300

    v25

--- HelloWorldBackEnd/src/com/hwb/HelloWorldMessage.java
+++ HelloWorldBackEnd/src/com/hwb/HelloWorldMessage.java
@@ -1,9 +1,25 @@
 package com.hwb;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class HelloWorldMessage {
-	private String message = "Version 24";
+	private String message = "Version 25";
 
 	public String getMessage() {
 		return this.message;
+	}
+
+	public String getEnvironment() {
+		Properties properties = new Properties();
+		InputStream is = this.getClass().getResourceAsStream("helloWorld.properties");
+		try {
+			properties.load(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return properties.getProperty("environment");
 	}
 }
